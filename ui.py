fb5fe091940978fcdff068e932c0cabe486b5354
TREE Industries <30479526+TREE-Ind@users.noreply.github.com>
2023-03-27 11:21:21 -0700

    Adding clear chat log
    Adding a button to clear chat log, and also the ability to retrieve previous requests to easily run them again.

--- ui.py
+++ ui.py
@@ -19,16 +19,38 @@ class GPT4BlenderAssistantPanel(bpy.types.Panel):
         layout.operator("wm.gpt4_blender_assistant_voice", text="Execute by Voice", icon='PLUS')
         layout.prop(scene, "gpt4_blender_assistant_input", text="")
         layout.operator("wm.gpt4_blender_assistant_text", icon='PLUS')
-        
 
         layout.label(text="Chat Log:")
+        layout.operator("wm.gpt4_clear_chat_log", text="Clear Chat Log", icon='X')
         for item in scene.gpt4_chat_log:
-            layout.label(text="You: " + item.user_input)
+            copy_user_input_op = layout.operator("wm.gpt4_copy_user_input", text="You: " + item.user_input)
+            copy_user_input_op.user_input = item.user_input
             layout.label(text="GPT-4: " + item.gpt4_response)
+
+
+class GPT4ClearChatLogOperator(bpy.types.Operator):
+    bl_idname = "wm.gpt4_clear_chat_log"
+    bl_label = "Clear Chat Log"
+
+    def execute(self, context):
+        context.scene.gpt4_chat_log.clear()
+        return {'FINISHED'}
+
+class GPT4CopyUserInputOperator(bpy.types.Operator):
+    bl_idname = "wm.gpt4_copy_user_input"
+    bl_label = "Copy User Input and Paste to Input Field"
+    user_input: bpy.props.StringProperty(name="User Input")
+
+    def execute(self, context):
+        bpy.context.window_manager.clipboard = self.user_input
+        context.scene.gpt4_blender_assistant_input = self.user_input
+        return {'FINISHED'}
 
 def register():
     bpy.utils.register_class(GPT4ChatLogItem)
     bpy.utils.register_class(GPT4BlenderAssistantPanel)
+    bpy.utils.register_class(GPT4ClearChatLogOperator)
+    bpy.utils.register_class(GPT4CopyUserInputOperator)
     bpy.types.Scene.gpt4_blender_assistant_input = bpy.props.StringProperty(
         name="Input",
         description="Input text for GPT-4 Blender Assistant",
@@ -44,9 +66,11 @@ def register():
 def unregister():
     bpy.utils.unregister_class(GPT4ChatLogItem)
     bpy.utils.unregister_class(GPT4BlenderAssistantPanel)
+    bpy.utils.unregister_class(GPT4ClearChatLogOperator)
+    bpy.utils.unregister_class(GPT4CopyUserInputOperator)
     del bpy.types.Scene.gpt4_blender_assistant_input
     del bpy.types.Scene.gpt4_blender_assistant_output
     del bpy.types.Scene.gpt4_chat_log
 
 if __name__ == "__main__":
-    register()
+    register
